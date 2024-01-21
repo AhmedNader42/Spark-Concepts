b148529958313a506933899526cba8ba71c5c887
Ahmed Nader <ahmednader111@gmail.com>
2024-01-21 15:00:26 +0300

    Added explanation for Coalesce and Repartitions

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,9 +10,19 @@ object Main {
 
   private val numbers = sc.parallelize(1 to 10000000)
 
+  /*
+      Repartition will always result in an Exchange(Shuffle) of the data.
+      Repartition is used if you need to have a more uniform data distribution
+   */
   private val repartitionedNumbers = numbers.repartition(2)
   repartitionedNumbers.count()
 
+  /*
+    Coalesce on the other hand will only Exchange(Shuffle) data if the number of new partitions is higher than the current.
+    Otherwise, it will "Union" together partitions from parents.
+
+    Data distribution is not guaranteed to be uniform. Some partitions will be much bigger than others.
+   */
   private val coalescedNumbers = numbers.coalesce(2)
   coalescedNumbers.count()
 
